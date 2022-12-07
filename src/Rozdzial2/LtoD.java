365b6efdb2289ebed2efdcb98eb5d1578be41b92
Romaniecki_G <romanieckig@gmail.com>
2022-12-07 21:55:45 +0100

    2.15

---
+++ src/Rozdzial2/LtoD.java
@@ -0,0 +1,12 @@
+package Rozdzial2;
+
+public class LtoD {
+    public static void main(String args[]){
+        //automatyczna konwersja typu long do double
+        long L;
+        double D;
+        L = 3153464326423L;
+        D = L;
+        System.out.println("L i D: "+ L +" "+ D);
+    }
+}
