fb86954c17b00b02432a0498d4393bcd0274bd65
Romaniecki_G <romanieckig@gmail.com>
2022-12-07 22:09:54 +0100

    2.16

---
+++ src/Rozdzial2/DtoL.java
@@ -0,0 +1,14 @@
+package Rozdzial2;
+
+public class DtoL {
+    // *** TEn program nie zostanie skompilowany
+    public static void main (String args[]){
+        double d;
+        long l;
+        d = 2134824646.0;
+        l = d; //niedozwolone
+        System.out.println("L i D"+ d + l);
+
+
+    }
+}
