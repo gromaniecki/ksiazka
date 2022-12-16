6cd3ddb219d8197c4e078951673b2af8898b1b98
Romaniecki_G <romanieckig@gmail.com>
2022-12-16 21:03:31 +0100

    wszystko dziala

--- src/Rozdzial2/SystemEkspercki.java
+++ src/Rozdzial2/SystemEkspercki.java
@@ -1,30 +1,129 @@
 package Rozdzial2;
 import java.util.Scanner;
 
+import static java.lang.System.*;
+
 public class SystemEkspercki {
 
     public static void main(String [] args){
 
-        System.out.println("Witamy w systemie ekperckim pomagającym wybrać odpowiedni rodzaj maseczki do twojej twarzy");
-        System.out.println("Grzegorz Romaniecki - AHE 2002 \n \n");
+        out.println("Witamy w systemie ekperckim pomagającym wybrać odpowiedni rodzaj maseczki do twojej twarzy");
+        out.println("Grzegorz Romaniecki - AHE 2002 \n \n");
         String TakLubNie = "\nproszę o odpowiedź tak lub nie\n";
 
-        System.out.println("Czy masz skórę suchą?"+TakLubNie);
-        String Przetluszczona = ("Czy masz skórę przetłuszczoną?"+TakLubNie);
-        boolean odpowiedz1 = TakNie();
-        if (odpowiedz1)
-                System.out.println("dupa");
-        else
-            System.out.println("biskupa");
-
-
-
+        out.println("Czy masz cienka skore?"+TakLubNie);
+        boolean response = TakNie();
+        if (response) {
+            naczynka();
+        } else {
+            matowa();
+        }
     }
+
+    private static void matowa() {
+        out.println("Czy masz skore matowa? \nproszę o odpowiedź tak lub nie\n");
+        boolean response = TakNie();
+        if (response) {
+            skoraGladka();
+        } else {
+            naturalneSwiecenie();
+        }
+    }
+
+    private static void skoraGladka() {
+        out.println("Czy masz gladka skore? \nproszę o odpowiedź tak lub nie\n");
+        boolean response = TakNie();
+        if (response) {
+            ceraNormalna();
+        } else {
+            zmarszczki();
+        }
+    }
+
+    private static void zmarszczki() {
+        out.println("Czy masz widoczne zmarszczki? \nproszę o odpowiedź tak lub nie\n");
+        boolean response = TakNie();
+        if (response) {
+            ceraDojrzala();
+        } else {
+            ceraNormalna();
+        }
+    }
+
+    private static void ceraDojrzala() {
+        out.println("Masz cere dojrzala");
+    }
+
+    private static void ceraNormalna() {
+        out.println("Masz cere normalna");
+    }
+
+    private static void naturalneSwiecenie() {
+        out.println("Czy twoja skora naturalnie swieci? \nproszę o odpowiedź tak lub nie\n");
+        boolean response = TakNie();
+        if (response) {
+            ceraNaczyniowa();
+        } else {
+            zaskorniki();
+        }
+    }
+
+    private static void ceraNaczyniowa() {
+        out.println("Masz cere naczyniowa");
+    }
+
+    private static void naczynka() {
+        out.println("Czy masz widoczne naczynka? \nproszę o odpowiedź tak lub nie\n");
+        boolean response = TakNie();
+        if (response) {
+            zaskorniki();
+        } else {
+            ceraSucha();
+        }
+    }
+
+    private static void ceraSucha() {
+        out.println("Masz cere sucha");
+    }
+
+    private static void zaskorniki() {
+        out.println("Czy masz zaskorniki? \nproszę o odpowiedź tak lub nie\n");
+        boolean response = TakNie();
+        if (response) {
+            ceraTlusta();
+        } else {
+            zaczerwienienia();
+        }
+    }
+
+    private static void ceraTlusta() {
+        out.println("Masz cere tlusta");
+    }
+
+    private static void zaczerwienienia() {
+        out.println("Czy masz widoczne zaczerwnienienia? \nproszę o odpowiedź tak lub nie\n");
+        boolean response = TakNie();
+        if (response) {
+            ceraWrazliwa();
+        } else {
+            ceraMieszana();
+        }
+    }
+
+    private static void ceraMieszana() {
+        out.println("Masz cere mieszana");
+    }
+
+    private static void ceraWrazliwa() {
+        out.println("Masz cere wrazliwa");
+    }
+
+
 
     private static boolean TakNie() {
         for(;;) {
             String error ="proszę napisać tak lub nie";
-            Scanner scan = new Scanner(System.in);
+            Scanner scan = new Scanner(in);
             String text = scan.nextLine();
 
 
@@ -34,7 +133,7 @@ public class SystemEkspercki {
             else if (text.equals("nie")){
                 return false;
                 }
-            else System.out.println(error);
+            else out.println(error);
 
         }
     }
